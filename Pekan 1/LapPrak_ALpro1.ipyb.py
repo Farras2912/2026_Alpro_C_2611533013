4712e4903fb77f110bb63c430f6c183cb4664010
Farras2912 <mfarrasagustin@gmail.com>
2026-09-10 21:20:48 +0700

    Add files via upload

---
+++ Pekan 1/LapPrak_ALpro1.ipyb.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+komentar
+dua baris atau lebih
+Pertemuan 1 pratikum alpro
+"""
+print("Hello World")
+print()
+print("Departemen Informatika")
+print("Universitas Andalas")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini " + "dibuat oleh"
+ + "Muhammad Farras Agustin_2611533013")
